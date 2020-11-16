82d38a4d2a214038c75218f4d01f9de541090146
EvanYeager <74571661+EvanYeager@users.noreply.github.com>
2020-11-16 16:40:30 -0600

    Add files via upload

---
+++ Pathfinder.cpp
@@ -0,0 +1,379 @@
+#include "Pathfinder.h"
+#include "Kismet/GameplayStatics.h"
+#include "CharacterOMEGAPARENT.h"
+#include "GridBase_Base.h"
+#include "Private_Pathfinder.h"
+
+// Sets default values for this component's properties
+UPathfinder::UPathfinder()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UPathfinder::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AGridManager_Parent::StaticClass(), FoundActors);
+	if (FoundActors.Num() > 0)
+	{
+		GridManager = Cast<AGridManager_Parent>(FoundActors[0]);
+	}
+}
+
+
+// Called every frame
+void UPathfinder::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+
+FGridStruct* UPathfinder::GetTileStruct(FVector2D Tile)
+{
+	return GridManager->GridStruct.Find(Tile);
+}
+
+bool UPathfinder::IsTileOccupied(FVector2D Tile)
+{
+	return IsValid(GetTileStruct(Tile)->OccupiedByWhom);
+}
+
+bool UPathfinder::IsCurrentTileOccupiedByPlayer()
+{
+	return IsTileOccupied(CurrentTile) && GetTileStruct(CurrentTile)->OccupiedByWhom->isPlayer; // IsTileOccupied() is there to protect from nullptrs
+}
+
+bool UPathfinder::IsCurrentTileOccupiedByEnemy()
+{
+	return IsTileOccupied(CurrentTile) && GetTileStruct(CurrentTile)->OccupiedByWhom->isPlayer == false; // IsTileOccupied() is there to protect from nullptrs
+}
+
+TArray<FVector2D> UPathfinder::GetTileNeighbors(FVector2D Tile, bool IncludePlayers, bool IncludeEnemies)
+{
+	TArray<FVector2D> OutNeighbors;
+	TArray<FVector2D> CardinalDirections;
+
+	CardinalDirections.Add(FVector2D(0, 1));  // Up
+	CardinalDirections.Add(FVector2D(1, 0));  // Right
+	CardinalDirections.Add(FVector2D(0, -1)); // Down
+	CardinalDirections.Add(FVector2D(-1, 0)); // Left
+
+	for (FVector2D CardinalDirection : CardinalDirections)
+	{
+		FVector2D AdjacentTile = CardinalDirection + Tile;
+
+
+		// NEED TO FIGURE OUT HOW TO DO THIS
+		/*
+		bool IsTileValid = false;
+		if (MovementArea.Num() > 0)
+		{
+			// using movementarea
+			if (MovementArea.Contains(AdjacentTile))
+			{
+				IsTileValid = true;
+			}
+			
+		}
+		else
+		{
+			// not using movementarea
+			if (GetTileStruct(AdjacentTile))
+			{
+				IsTileValid = true;
+			}
+
+		}
+		*/
+
+		if (GetTileStruct(AdjacentTile)) // TODO maybe add another check here to only include tiles in movement range as a safeguard
+		{
+			if (IsTileOccupied(AdjacentTile))
+			{
+				FGridStruct* AdjacentTileStruct = GetTileStruct(AdjacentTile);
+				ACharacterOMEGAPARENT* Char = AdjacentTileStruct->OccupiedByWhom;
+				if (!IncludePlayers ^ Char->isPlayer || !IncludeEnemies ^ !Char->isPlayer) // Returns true if both booleans are the same, i.e. char is a player and include players, or if an enemy and include enemies
+				{
+					OutNeighbors.AddUnique(AdjacentTile);
+				}
+			}
+			else
+			{
+				OutNeighbors.AddUnique(AdjacentTile);
+			}
+		}
+	}
+	return OutNeighbors;
+}
+
+
+TArray<FVector2D> UPathfinder::BreadthSearch(FVector2D StartTile, int32 CharacterMovement, TArray<FVector2D>& TilesToMakeRed, TArray<ACharacterOMEGAPARENT*>& CharsInRange, bool IsPlayerCalling)
+{
+	ClearMemberVariables();
+	MaxMovement = CharacterMovement;
+	NumOfPossibleTiles = GetMovementOptionArea(MaxMovement + 1); // The + 1 is so it will search for the red highlights
+	IsPlayerPathfinding = IsPlayerCalling;
+	Queue.Add(StartTile);
+	ValidTiles.AddUnique(StartTile);
+	ResetFinalCosts();
+
+	for (int32 i = 1; i <= NumOfPossibleTiles; i++)
+	{
+		if (Queue.Num() > 0)
+		{
+			for (FVector2D NeighborTile : GetTileNeighbors(Queue[0]))
+			{
+				CurrentTile = NeighborTile;
+				EvaluateCurrentTileForBreadthSearch();
+			}
+			Queue.RemoveAt(0);
+		}
+	}
+	TilesToMakeRed = RedTiles;
+	CharsInRange = CharsFound;
+	return ValidTiles;
+}
+
+void UPathfinder::ClearMemberVariables()
+{
+	MovementArea.Empty();
+	Queue.Empty();
+	ValidTiles.Empty();
+	RedTiles.Empty();
+	CharsFound.Empty();
+	Path.Empty();
+	Queue.Empty();
+	EstablishedTiles.Empty();
+	FinalCostFromCurrentNeighbor = 0;
+}
+
+int32 UPathfinder::GetMovementOptionArea(int32 Movement)
+{
+	return ((Movement * 2) + 2) * (Movement + 1) - ((Movement * 2) + 1);
+}
+
+void UPathfinder::ResetFinalCosts()
+{
+	for(auto& Elem : GridManager->GridStruct)
+	{
+		GetTileStruct(Elem.Key)->FinalCost = 0;
+	}
+}
+
+void UPathfinder::EvaluateCurrentTileForBreadthSearch()
+{
+	CurrentTileStruct = GetTileStruct(CurrentTile);
+	MoveCost = GetTileStruct(Queue[0])->FinalCost + CurrentTileStruct->TileMovementCost;
+	if (TileShouldBeRed())
+	{
+		RedTiles.AddUnique(CurrentTile);
+		if (EnemyIsPathfindingAndCurrentTileIsOccupiedByPlayer())
+		{
+			CharsFound.AddUnique(CurrentTileStruct->OccupiedByWhom);
+		}
+	}
+	else if (CurrentTileIsWalkable())
+	{
+		AddCurrentTileToValidTiles();
+	}
+}
+
+bool UPathfinder::TileShouldBeRed()
+{
+	return MoveCost > MaxMovement || // Tile outside movement range
+	CurrentTileStruct->TerrainType == Impossible ||
+	PlayerIsPathfindingAndCurrentTileIsOccupiedByEnemy() ||
+	EnemyIsPathfindingAndCurrentTileIsOccupiedByPlayer();
+}
+
+bool UPathfinder::PlayerIsPathfindingAndCurrentTileIsOccupiedByEnemy()
+{
+	return IsPlayerPathfinding && IsCurrentTileOccupiedByEnemy();
+}
+
+bool UPathfinder::EnemyIsPathfindingAndCurrentTileIsOccupiedByPlayer()
+{
+	return !IsPlayerPathfinding && IsCurrentTileOccupiedByPlayer();
+}
+
+void UPathfinder::AddCurrentTileToValidTiles()
+{
+	Queue.AddUnique(CurrentTile);
+	CurrentTileStruct->FinalCost = MoveCost;
+	ValidTiles.AddUnique(CurrentTile);
+}
+
+bool UPathfinder::CurrentTileIsWalkable()
+{
+	return MoveCost <= MaxMovement && (!ValidTiles.Contains(CurrentTile) || 
+	MoveCost < CurrentTileStruct->FinalCost);
+}
+
+/* ==========================================
+	Breadth Search ends and Find Path starts	
+========================================== */
+
+
+TArray<FVector2D> UPathfinder::FindPathToTarget(TArray<FVector2D> AvailableMovementArea, FVector2D StartTile, FVector2D TargetTile, bool IsPlayerCalling)
+{
+	ClearMemberVariables();
+	ResetPathfindingTileVars();
+	MovementArea = AvailableMovementArea;
+	InitialTile = StartTile;
+	CurrentTile = InitialTile;
+	DestinationTile = TargetTile;
+	IsPlayerPathfinding = IsPlayerCalling;
+	int32 EstimatedCost = GetEstimatedCostToTile(DestinationTile);
+	CurrentTileStruct = GetTileStruct(CurrentTile);
+	CurrentTileStruct->FinalCost = EstimatedCost;
+	CurrentTileStruct->EstimatedCostToTarget = EstimatedCost;
+	Queue.AddUnique(InitialTile);
+
+	Pathfind();
+	return Path;
+}
+
+void UPathfinder::ResetPathfindingTileVars()
+{
+	for (auto& Element : GridManager->GridStruct)
+	{
+		FGridStruct* TileStruct = GetTileStruct(Element.Key);
+		TileStruct->FinalCost = 0;
+		TileStruct->CostFromStart = 0;
+		TileStruct->EstimatedCostToTarget = 0;
+		TileStruct->PreviousTileCoords = FVector2D(0, 0);
+	}
+}
+
+void UPathfinder::Pathfind()
+{
+	while (Queue.Num() > 0)
+	{
+		CurrentTile = FindCheapestInQueue();
+		Queue.Remove(CurrentTile);
+		EstablishedTiles.AddUnique(CurrentTile);
+		TArray<FVector2D> Neighbors = GetTileNeighbors(CurrentTile, IsPlayerPathfinding, !IsPlayerPathfinding);
+		for (FVector2D CurrentNeighbor : Neighbors)
+		{
+			if (!EstablishedTiles.Contains(CurrentNeighbor) || FinalCostFromCurrentNeighbor < GetTileStruct(CurrentNeighbor)->CostFromStart)
+			{
+				FinalCostFromCurrentNeighbor = GetTileStruct(CurrentTile)->CostFromStart + GetTileStruct(CurrentNeighbor)->TileMovementCost;
+				SetTileVariables(CurrentTile, CurrentNeighbor);
+				if (CurrentNeighbor == DestinationTile)
+				{
+					Path = RetracePath();
+					return;
+				}
+				Queue.AddUnique(CurrentNeighbor);
+			}
+		}
+	}
+}
+
+int32 UPathfinder::GetEstimatedCostToTile(FVector2D Tile)
+{
+	FVector2D TempDistance = Tile - DestinationTile;
+	FVector2D FinalDistance = FVector2D(abs(TempDistance.X), abs(TempDistance.Y));
+	return int32(FinalDistance.X + FinalDistance.Y);
+}
+
+FVector2D UPathfinder::FindCheapestInQueue()
+{
+	FVector2D QueueCheapest = Queue[0];
+	for (FVector2D Element : Queue)
+	{
+		FGridStruct* QueueElemStruct = GetTileStruct(Element);
+		FGridStruct* QueueCheapestStruct = GetTileStruct(QueueCheapest);
+		if (QueueElementIsBetterThanQueueCheapest(Element, QueueCheapest))
+		{
+			QueueCheapest = Element;
+		}
+	}
+	return QueueCheapest;
+}
+
+bool UPathfinder::QueueElementIsBetterThanQueueCheapest(FVector2D QueueElem, FVector2D QueueCheapest)
+{
+	FGridStruct* QueueElemStruct = GetTileStruct(QueueElem);
+	FGridStruct* QueueCheapestStruct = GetTileStruct(QueueCheapest);
+	// Conditions for being true: QueueElem FinalCost is less than QueueCheapest's, or if they are equal but QueueElem is closer to destination
+	return (QueueElemStruct->FinalCost < QueueCheapestStruct->FinalCost) || 
+	(QueueElemStruct->FinalCost == QueueCheapestStruct->FinalCost && 
+	QueueElemStruct->EstimatedCostToTarget < QueueCheapestStruct->EstimatedCostToTarget);
+}
+
+void UPathfinder::SetTileVariables(FVector2D PreviousTile, FVector2D CurrentNeighbor)
+{
+	FGridStruct* CurrentNeighborStruct = GetTileStruct(CurrentNeighbor);
+	CurrentNeighborStruct->FinalCost = FinalCostFromCurrentNeighbor + GetEstimatedCostToTile(CurrentNeighbor);
+	CurrentNeighborStruct->CostFromStart = FinalCostFromCurrentNeighbor;
+	CurrentNeighborStruct->EstimatedCostToTarget = GetEstimatedCostToTile(CurrentNeighbor);
+	CurrentNeighborStruct->PreviousTileCoords = PreviousTile;
+}
+
+TArray<FVector2D> UPathfinder::RetracePath()
+{
+	TArray<FVector2D> InvertedPath;
+	TArray<FVector2D> OutPath;
+	CurrentTile = DestinationTile;
+
+	while (CurrentTile != InitialTile)
+	{
+		InvertedPath.AddUnique(CurrentTile);
+		CurrentTile = GetTileStruct(CurrentTile)->PreviousTileCoords;
+	}
+	for (int32 i = InvertedPath.Num() - 1; i >= 0; i--)
+	{
+		OutPath.AddUnique(InvertedPath[i]);
+	}
+	return OutPath;
+}
+
+
+
+TArray<FVector2D> UPathfinder::FindPathAsEnemy(FVector2D StartTile, TArray<FVector2D> PossibleTargetTiles)
+{
+	TArray<FVector2D> CurrentPath,
+	BestPath;
+	int32 CurrentPathCost,
+	BestPathCost = 999;
+	for(FVector2D TargetTileElement : PossibleTargetTiles)
+	{
+		TArray<FVector2D> Argument;
+		CurrentPath = FindPathToTarget(Argument, StartTile, TargetTileElement, false);
+		CurrentPathCost = GetTileStruct(CurrentPath.Last())->FinalCost; // Make sure to keep the pathfinding variables non-zero after pathfinding so I can use this outside the function
+		if (IsThisFirstEnemyPathfind(BestPath))
+		{
+			BestPath = CurrentPath;
+			BestPathCost = GetTileStruct(BestPath.Last())->FinalCost;
+		}
+		else if (CurrentPathCost < BestPathCost)
+		{
+			BestPath = CurrentPath;
+			BestPathCost = GetTileStruct(BestPath.Last())->FinalCost;
+		}
+	}
+	return BestPath;
+}
+
+bool UPathfinder::IsThisFirstEnemyPathfind(TArray<FVector2D> BestPath)
+{
+	return !BestPath.IsValidIndex(0);
+}
+
+
+
+
+
+
+
+void UPathfinder::PrintInLog()
+{
+	
+}
---
+++ Pathfinder.h
@@ -0,0 +1,125 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlayerController_Parent.h"
+#include "GridManager_Parent.h"
+#include "Pathfinder.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TOPDOWN_API UPathfinder : public UActorComponent
+{
+	GENERATED_BODY()
+
+private:
+	/**
+	 * 
+	 * Private Functions
+	 * 
+	*/
+
+	// BreadthSearch
+	int32 GetMovementOptionArea(int32 Movement);
+	void ResetFinalCosts();
+	// Evaluates tile and adds it to appropriate arrays like ValidTiles, TilesToMakeRed, and also populates the CharsInRange array
+	void EvaluateCurrentTileForBreadthSearch();
+	bool TileShouldBeRed();
+	bool EnemyIsPathfindingAndCurrentTileIsOccupiedByPlayer();
+	bool PlayerIsPathfindingAndCurrentTileIsOccupiedByEnemy();
+	void AddCurrentTileToValidTiles();
+	bool CurrentTileIsWalkable();
+
+	// FindPathToTarget
+	void Pathfind();
+	int32 GetEstimatedCostToTile(FVector2D Tile);
+	FVector2D FindCheapestInQueue();
+	bool QueueElementIsBetterThanQueueCheapest(FVector2D QueueElem, FVector2D QueueCheapest);
+	void SetTileVariables(FVector2D PreviousTile, FVector2D CurrentNeighbor);
+	bool IsThisFirstEnemyPathfind(TArray<FVector2D> BestPath);
+
+	// Both
+	void ClearMemberVariables();
+
+
+	/**
+	 * 
+	 * Member Variables
+	 * 
+	*/
+
+	// BreadthSearch
+	TArray<ACharacterOMEGAPARENT*> CharsFound;
+	int32 NumOfPossibleTiles;
+
+	// FindPathToTarget
+	TArray<FVector2D> MovementArea;
+	FVector2D InitialTile;
+	FVector2D DestinationTile;
+	TArray<FVector2D> Path;
+	TArray<FVector2D> EstablishedTiles;
+	int32 FinalCostFromCurrentNeighbor;
+
+	// BreadthSearch and FindPathToTarget
+	TArray<FVector2D> Queue;
+	FVector2D CurrentTile;
+	FGridStruct* CurrentTileStruct;
+	TArray<FVector2D> ValidTiles;
+	int32 MaxMovement;
+	int32 MoveCost;
+	TArray<FVector2D> RedTiles;
+	bool IsPlayerPathfinding;
+	
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Sets default values for this component's properties
+	UPathfinder();
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	AGridManager_Parent* GridManager;
+
+	UPROPERTY(EditAnywhere)
+	APlayerController* Owner = Cast<APlayerController>(GetOwner());
+
+	bool IsTileOccupied(FVector2D Tile);
+
+	bool IsCurrentTileOccupiedByPlayer();
+
+	UFUNCTION(BlueprintCallable, BluePrintPure, Category = "Utility")
+	bool IsCurrentTileOccupiedByEnemy();
+
+	FGridStruct* GetTileStruct(FVector2D Tile);
+
+	UFUNCTION(BlueprintCallable, BluePrintPure)
+	TArray<FVector2D> GetTileNeighbors(FVector2D Tile, bool IncludePlayers = true, bool IncludeEnemies = true);
+
+	UFUNCTION(BlueprintCallable, Category = "Grid Struct")
+	void ResetPathfindingTileVars();
+
+	UFUNCTION(BlueprintCallable)
+	// Returns: Tiles outside movement range to make red if player is calling, Chars found if enemy is calling, and return value is the valid movement area.
+	TArray<FVector2D> BreadthSearch(FVector2D StartTile, int32 CharacterMovement, TArray<FVector2D>& TilesToMakeRed, TArray<ACharacterOMEGAPARENT*>& CharsFound, bool IsPlayerCalling = true);
+
+	TArray<FVector2D> RetracePath();
+
+	UFUNCTION(BlueprintCallable)
+	// IsPlayerCalling is true if a player character is pathfinding, false if an enemy is. This way if an enemy calls this they can go through other enemies.
+	TArray<FVector2D> FindPathToTarget(TArray<FVector2D> AvailableMovementArea, FVector2D StartTile, FVector2D TargetTile, bool IsPlayerCalling = true); 
+
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector2D> FindPathAsEnemy(FVector2D StartTile, TArray<FVector2D> PossibleTargetTiles);
+		
+
+
+
+	UFUNCTION(BlueprintCallable)
+	void PrintInLog();
+
+
+
+};
